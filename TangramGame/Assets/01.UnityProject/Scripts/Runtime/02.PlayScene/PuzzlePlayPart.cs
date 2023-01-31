5af7cf14c2c9121c8e6f14f6ae4c622d6d52f70c
kimsm23236 <kimsm23236@gmail.com>
2023-01-31 10:13:37 +0900

    [SuminKim][feat] -> Drag ok

--- TangramGame/Assets/01.UnityProject/Scripts/Runtime/02.PlayScene/PuzzlePlayPart.cs
+++ TangramGame/Assets/01.UnityProject/Scripts/Runtime/02.PlayScene/PuzzlePlayPart.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 using UnityEngine.EventSystems;
 
-public class PuzzlePlayPart : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerMoveHandler
+public class PuzzlePlayPart : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
 {
     private bool isClicked = false;
+    private RectTransform objRect = default;
+    private PuzzleInitZone puzzleInitZone = default;
     // Start is called before the first frame update
     void Start()
     {
         isClicked = false;
+        objRect = gameObject.GetRect();
+        puzzleInitZone = transform.parent.gameObject.GetComponentMust<PuzzleInitZone>();
     }
 
     // Update is called once per frame
@@ -22,23 +26,26 @@ public class PuzzlePlayPart : MonoBehaviour, IPointerDownHandler, IPointerUpHand
     public void OnPointerDown(PointerEventData eventData)
     {
         isClicked = true;
-        GFunc.Log($"{gameObject.name} 을 선택했다.");
     }
     // 마우스 버튼에서 손을 뗐을 때 동작하는 함수
     public void OnPointerUp(PointerEventData eventData)
     {
         isClicked = false;
-        GFunc.Log($"{gameObject.name} 을 선택 취소했다.");
     }
-    // 마우스를 드래그 중일 때 동작하는 함수
-    public void OnPointerMove(PointerEventData eventData)
-    {
-        if(isClicked == true)
-        {
-            GFunc.Log($"마우스 포지션 눈으로 확인 ({eventData.position.x} {eventData.position.y})");
-        }
-        
-        
 
+    public void OnDrag(PointerEventData eventData)
+    {
+        gameObject.AddAnchoredPos(eventData.delta / puzzleInitZone.parentCanvas.scaleFactor);
     }
+
+    // Lagacy Code
+    // 마우스를 드래그 중일 때 동작하는 함수
+    // public void OnPointerMove(PointerEventData eventData)
+    // {
+    //     if(isClicked == true)
+    //     {
+    //         GFunc.Log($"마우스 포지션 눈으로 확인 ({eventData.position.x} {eventData.position.y})");
+    //     }
+    // }
+
 }
